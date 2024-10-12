514c661fe34524b3799f7ef4dd477863b6ff0b97
sampad <sampadsingha8@gmail.com>
2024-10-13 03:05:47 +0600

    commit 10

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,17 @@
+import {FaSquareInstagram} from "react-icons/fa";
+import {FaFacebook} from "react-icons/fa";
+import {FaLinkedin} from "react-icons/fa";
 
 
 const Footer = () => {
     return (
-        <div>
-            <h3>Footer</h3>
+        <div className="footer">
+            <h5>Follow me on social media</h5>
+            <div className="footer-links">
+                <a href=""><FaSquareInstagram />  Instagram</a>
+                <a href=""><FaFacebook />  Facebook</a>
+                <a href=""><FaLinkedin /> LinkedIn</a>
+            </div>
             <p>©2025 Shahriar Ahmed</p>
         </div>
     );
