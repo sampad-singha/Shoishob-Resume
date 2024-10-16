f83e12c593cd05882e6b08f987982e207fb0555d
sampad <sampadsingha8@gmail.com>
2024-10-17 02:45:26 +0600

    POST Production: 404 page

--- src/components/NotFoundPage404.jsx
+++ src/components/NotFoundPage404.jsx
@@ -1,8 +1,10 @@
 import { Typography} from "@material-tailwind/react";
 import { FlagIcon } from "@heroicons/react/24/solid";
-import {Link} from "react-router-dom";
+import {Link, useRouteError} from "react-router-dom";
 
 export function NotFoundPage404() {
+    const error = useRouteError();
+    const status = error?error.status:0;
     return (
         <div className="h-screen mx-auto grid place-items-center text-center px-8 nf">
             <div>
@@ -12,7 +14,7 @@ export function NotFoundPage404() {
                     color="blue-gray"
                     className="mt-10 !text-3xl !leading-snug md:!text-4xl"
                 >
-                    Error 404 <br /> It looks like something went wrong.
+                    Error {status?status:"Unknown"} <br /> {status?`Page Not Found`:`It looks like something went wrong.`}
                 </Typography>
                 <Typography className="mt-8 mb-14 text-[18px] font-normal text-gray-500 mx-auto md:max-w-sm">
                     Don&apos;t worry, our team is already on it.Please try refreshing
